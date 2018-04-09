c23463c315a824f10254eb8ded922a3dd5e3e412
Langxxx <316230604@qq.com>
2018-04-09 15:17:40 +0800

    Initial Commit

---
+++ ModulePresenter/ModulePresenter.h
@@ -0,0 +1,19 @@
+//
+//  ModulePresenter.h
+//  ModulePresenter
+//
+//  Created by lang on 09/04/2018.
+//  Copyright © 2018 Beary Innovative. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ModulePresenter.
+FOUNDATION_EXPORT double ModulePresenterVersionNumber;
+
+//! Project version string for ModulePresenter.
+FOUNDATION_EXPORT const unsigned char ModulePresenterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ModulePresenter/PublicHeader.h>
+
+
